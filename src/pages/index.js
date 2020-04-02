e3a6142e03c1be37b15d24ea7d3c99fd099264ca
lorenzwolfe <lfwolfe@gmail.com>
2020-04-01 22:49:32 -0400

    add css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from "react"
 import { Layout } from "../components/Layout"
-import "../css/test2.scss"
-
-export default () => <Layout>Hello</Layout>
+import { SimpleHero } from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
+import { About } from "../components/Home/About"
+import { Services } from "../components/Home/Services"
+export default () => (
+  <Layout>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="ndustry. Lorem Ipsum has been the industry's standard"
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+    <About />
+    <Services />
+  </Layout>
+)
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -3,6 +3,11 @@ import { Layout } from "../components/Layout"
 import "./layout.css"
 export default class tours extends Component {
   render() {
-    return <Layout>hello from tours page</Layout>
+    return (
+      <Layout>
+        hello from tours page
+        <div></div>
+      </Layout>
+    )
   }
 }
